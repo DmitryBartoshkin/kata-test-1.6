4552ba9a59aaac83a5bccd4488d55cb5e6800f99
Dmitry Bartoshkin <bartoshkin@gmail.com>
2023-11-19 20:07:19 +0300

    Add a first block of content main-page

---
+++ src/scss/blocks/read-more/read-more.js
@@ -0,0 +1,7 @@
+let clickElement = document.querySelector('.read-more');
+let content = document.querySelector('.text-content');
+
+clickElement.addEventListener('click', function() {
+    content.classList.add('text-content--show-full');
+    clickElement.classList.add('read-more--hidden');
+});
